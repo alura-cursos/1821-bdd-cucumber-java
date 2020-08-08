4d624305145dd33e776165d54156d0690825def3
Nico Steppat <steppat@gmail.com>
2020-08-08 13:06:41 +0200

    minors

--- src/main/java/br/com/alura/leilao/controller/DBController.java
+++ src/main/java/br/com/alura/leilao/controller/DBController.java
@@ -34,7 +34,7 @@ public class DBController {
 	@Autowired
 	PasswordEncoder encoder;
 	
-	@GetMapping("/db")
+	@GetMapping("/delete")
 	public ResponseEntity<String> deleta() {
 		lances.deleteAll();
 		leiloes.deleteAll();

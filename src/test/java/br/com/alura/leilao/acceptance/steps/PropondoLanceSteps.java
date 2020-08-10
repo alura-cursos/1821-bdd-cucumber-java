b94be1da594e13381a7cc6d4a50fbcb16f4e2614
Nico Steppat <steppat@gmail.com>
2020-08-10 13:15:15 +0200

    video 4.4

--- src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -2,13 +2,15 @@ package br.com.alura.leilao.acceptance.steps;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-import org.apache.bcel.generic.LNEG;
 import org.junit.Assert;
 
 import br.com.alura.leilao.model.Lance;
 import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.pt.Dado;
@@ -21,37 +23,35 @@ public class PropondoLanceSteps {
 	private Leilao leilao;
 
 	private ArrayList<Lance> lista;
-	
-	
+
 	@Before
 	public void setup() {
 		this.lista = new ArrayList<Lance>();
 		leilao = new Leilao("Tablet XPTO");
 	}
-	
+
 	@After
 	public void tearDown() {
 //		System.out.println("after");
 	}
-	
-	
+
 	@Dado("um lance valido")
 	public void dado_um_lance_valido() {
 		Usuario usuario = new Usuario("fulano");
-		lance = new Lance(usuario , BigDecimal.TEN);
+		lance = new Lance(usuario, BigDecimal.TEN);
 	}
 
 	@Quando("propoe ao leilao")
 	public void quando_propoe_o_lance() {
 		leilao.propoe(lance);
 	}
-	
+
 	@Entao("o lance eh aceito")
 	public void entao_o_lance_eh_aceito() {
 		Assert.assertEquals(1, leilao.getLances().size());
 		Assert.assertEquals(BigDecimal.TEN, leilao.getLances().get(0).getValor());
 	}
-	
+
 //	@Dado("varios lances validos")
 //	public void varios_lances_validos() {
 //		Usuario usuario1 = new Usuario("fulano");
@@ -60,19 +60,18 @@ public class PropondoLanceSteps {
 //		lance15 = new Lance(usuario2 , new BigDecimal("15.0"));
 //		leilao = new Leilao("Tablet XPTO");
 //	}
-	
+
 	@Dado("um lance de {double} reais do usuario {string}")
 	public void um_lance_de_reais_do_usuario_fulano(Double valor, String nomeUsuario) {
 		Lance lance = new Lance(new Usuario(nomeUsuario), new BigDecimal(valor));
-	    lista.add(lance);
+		lista.add(lance);
 	}
-	
+
 	@Quando("propoe varios lances ao leilao")
 	public void propoe_varios_lances_ao_leilao() {
 		this.lista.forEach(lance -> leilao.propoe(lance));
 	}
-	
-	
+
 	@Entao("os lances sao aceitos")
 	public void os_lances_sao_aceitos() {
 		Assert.assertEquals(this.lista.size(), leilao.getLances().size());
@@ -80,22 +79,32 @@ public class PropondoLanceSteps {
 		Assert.assertEquals(this.lista.get(1).getValor(), leilao.getLances().get(1).getValor());
 	}
 
-	
 	@Dado("um lance invalido de {double} reais e do usuario {string}")
 	public void um_lance_de_reais(Double valor, String nomeUsuario) {
-		System.out.println(nomeUsuario);
+//		System.out.println(nomeUsuario);
 		this.lance = new Lance(new BigDecimal(valor));
 	}
-	
+
 	@Entao("o lance nao eh aceito")
 	public void o_lance_nao_eh_aceito() {
 		Assert.assertEquals(0, leilao.getLances().size());
 	}
-	
-	
+
+	@Dado("dois lances")
+	public void dois_lances(DataTable dataTable) {
+		List<Map<String, String>> valores = dataTable.asMaps();
+		for (Map<String, String> mapa : valores) {
+			String valor = mapa.get("valor");
+			String nome = mapa.get("nomeUsuario");
+			Lance lance = new Lance(new Usuario(nome), new BigDecimal(valor));
+			lista.add(lance);
+		}
+	}
+
+	@Entao("o segundo lance nao eh aceito")
+	public void o_segundo_lance_nao_eh_aceito() {
+		Assert.assertEquals(1, leilao.getLances().size());
+		Assert.assertEquals(this.lista.get(0).getValor(), leilao.getLances().get(0).getValor());
+	}
+
 }
-
-
-
-
-
--- src/test/resources/features/propondo_lances.feature
+++ src/test/resources/features/propondo_lances.feature
@@ -21,5 +21,17 @@ Esquema do Cenario: Propondo um lance invalido
 Exemplos:
     | valor | nomeUsuario |
     |    0  | beltrano |
-    |    -1 | cigano |
- 
+    |   -1  | cigano |
+    
+
+Cenario: Propondo uma sequencia de lances
+ Dado dois lances
+    | valor | nomeUsuario |
+    |   10.0  | beltrano  |   
+    |   15.0  | beltrano  |		
+ Quando propoe varios lances ao leilao
+ Entao o segundo lance nao eh aceito
+    
+    
+    
+    
